642506411adec4af6ec0684ed480fe854f573e70
Zoltan Sasvari <sasvarizoli@me.com>
2020-04-04 15:53:22 +0200

    basic GET endpoint

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,17 @@
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello Startup Safari 2020!", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
