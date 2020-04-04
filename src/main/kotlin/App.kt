69f49959b33d8594a51ea2f1d7f9aab3891b9539
Zoltan Sasvari <sasvarizoli@me.com>
2020-04-04 16:51:32 +0200

    basic HTML landing page

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -5,12 +5,24 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.html.*
+import kotlinx.html.stream.appendHTML
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080) {
         routing {
             get("/") {
-                call.respondText("Hello Startup Safari 2020!", ContentType.Text.Html)
+                val html = buildString {
+                    appendHTML().html {
+                        head {
+                            title(content = "FeedbackPro")
+                        }
+                        body {
+                            h1 { +"FeedbackPro" }
+                        }
+                    }
+                }
+                call.respondText(html, ContentType.Text.Html)
             }
         }
     }.start(wait = true)
